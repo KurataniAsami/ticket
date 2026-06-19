2306f60ff0e17464d2cf1538926bb6165a743f0c
kurataniasami <kr-scream.hurryx@ezweb.ne.jp>
2026-06-19 15:49:04 +0900

    アーティスト画像の取得

---
+++ app/api/spotify/artist/route.ts
@@ -0,0 +1,57 @@
+import { prisma } from "@/app/libs/prisma";
+import { request } from "http";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url)
+
+  const artist = searchParams.get("artist")
+
+  if(!artist) {
+    return NextResponse.json(
+      { error: 'アーティストが見つかりません'},
+      { status: 400 }
+    )
+  }
+
+  // アクセストークン取得
+  const tokenResponse = await fetch(
+    "https://accounts.spotify.com/api/token",
+    {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "grant_type=client_credentials",
+    }
+  )
+
+  const tokenData = await tokenResponse.json()
+
+  // artistを検索してDBに保存？
+  const searchResponse = await fetch(
+    `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+      artist
+    )}&type=artist&limit=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${tokenData.access_token}`,
+      },
+    }
+  )
+
+  const searchData = await searchResponse.json()
+
+  const artistData = searchData.artists.items[0]
+
+  return NextResponse.json({
+    name: artistData.name,
+    imageUrl: artistData.images?.[0]?.url ?? null,
+    spotifyUrl: artistData.external_urls.spotify,
+  })
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.jp',
+      },
+      // vrtbyludcvlofrmipmbx.supabase.coはsupabaseのプロジェクトid
+      {
+        protocol: 'https',
+        hostname: 'vrtbyludcvlofrmipmbx.supabase.co',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
